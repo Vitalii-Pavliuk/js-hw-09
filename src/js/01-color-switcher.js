002ba8e5fb9575a0383b95ce801b7b2edec679fc
Morello100 <114472069+Morello100@users.noreply.github.com>
2025-03-05 13:03:18 +0200

    Add/color-switcher Fix/package-json

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+document.querySelector('button[data-start]').addEventListener('click', onButtonStart);
+document.querySelector('button[data-stop]').addEventListener('click', onButtonStop);
 
+let intervalId = null;
+let isActive = false;
+
+function onButtonStart() {
+    if (isActive) return;
+
+    intervalId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+      }, 1000);
+
+    isActive = true;
+}
+
+function onButtonStop() {
+    clearInterval(intervalId);
+    isActive = false;
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+   
